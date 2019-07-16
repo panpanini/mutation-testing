f67b9963ac6de81288735c89a518746f3cb2f398
panpanini <panpanini@mercari.com>
2019-07-16 21:46:24 +0800

    rename jacocoReport -> jacoco

--- jacoco.sh
+++ jacoco.sh
@@ -1,2 +1,2 @@
-./gradlew jacocoCoverageReport
+./gradlew clean app:jacocoCoverageReport
 open file:///Users/panpanini/development/slides/mutation_testing/app/build/reports/jacocoCoverageReport/html/jp.co.panpanini.session/SessionController.kt.html
